c07f39e1ce0260b3373fb6c7e281e76aef7f0575
Vladislav Sapozhnikov <vlad.sapozhnickof@gmail.com>
2023-12-15 14:20:45 +0300

    Table in process

--- tablewidget.cpp
+++ tablewidget.cpp
@@ -3,16 +3,53 @@
 TableWidget::TableWidget(QWidget *parent)
     : QTableWidget{parent}
 {
-    start();
+    QList<QList<Resistor *>> list;
+
+    QList<Resistor *> first;
+    first.append(new Resistor(10, 10, true));
+    first.append(new Resistor(20, 20, true));
+    first.append(new Resistor(30, 30, false));
+    first.append(new Resistor(50, 50, false));
+
+    list.append(first);
+
+    drawTable(list);
+    resizeColumnsToContents();
 }
 
-void TableWidget::start()
+void TableWidget::drawTable(QList<QList<Resistor *>> dList)
 {
+    qDebug() << "Кол-во строк: " << dList.size();
 
 
-    setColumnCount(5);
-    setRowCount(5);
+    setColumnCount(1 + dList.first().size());
+    setRowCount(dList.size() + 2);
 
-    QTableWidgetItem *item = new QTableWidgetItem("ewekjgnjklwegnklwegklkl");
-    setItem(0, 0, item);
+    drowHeaderLine(dList.first().size());
+    for (int i = 0; i < dList.size(); i++) drowDataRow(i, dList.at(i));
+}
+
+void TableWidget::drowDataRow(int rowNumber, QList<Resistor *> list)
+{
+    for(int columnNumber = 0; columnNumber < list.size(); columnNumber++) {
+        drawCell(rowNumber+1, 0, QString::number(rowNumber+1));
+        drawCell(rowNumber+1, columnNumber+1, list.at(columnNumber)->isActive()? "+" : "-");
+    }
+}
+
+void TableWidget::drowHeaderLine(int size)
+{
+    qDebug() << "Отрисовка заглавной строки";
+    drawCell(0, 0, "Кол-во % мощности,\nкоторое потребляет\nполезная нагрузка");
+    for (int i = 1; i <= size; i++) drawCell(0, i, QString::number(i));
+
+    this->horizontalHeader()->setSectionResizeMode(QHeaderView::Stretch);
+    this->verticalHeader()->setSectionResizeMode(QHeaderView::ResizeToContents);
+}
+
+void TableWidget::drawCell(int row, int column, QString text)
+{
+    auto* item = new QTableWidgetItem(text);
+    item->setFlags(item->flags() & ~Qt::ItemIsEditable & ~Qt::ItemIsSelectable);
+    setItem(row, column, item);
 }
--- tablewidget.h
+++ tablewidget.h
@@ -2,6 +2,11 @@
 #define TABLEWIDGET_H
 
 #include <QTableWidget>
+#include <QList>
+#include <QDebug>
+#include <QHeaderView>
+
+#include "resistor.h"
 
 class TableWidget : public QTableWidget
 {
@@ -9,7 +14,12 @@ class TableWidget : public QTableWidget
 public:
     explicit TableWidget(QWidget *parent = nullptr);
 
-    void start();
+    void drawTable(QList<QList<Resistor *>> dList);
+    void drawCell(int row, int column, QString text);
+
+    void drowDataRow(int row, QList<Resistor *> list);
+
+    void drowHeaderLine(int size);
 
 signals:
 
